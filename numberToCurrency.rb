ebb13f560e2e65fd52251068e699a92dbfaf1612
yeyangever <yeyangever@gmail.com>
2013-01-24 23:21:31 -0800

    add number currency converter

---
+++ numberToCurrency.rb
@@ -0,0 +1,72 @@
+def numberToCurrency(num)
+  if !num.is_a? Integer
+    puts "Input #{num} is not a integer, return"
+    return
+  end
+
+  def roundQuotential(quot)
+    if(quot > 10)
+      return quot.round(0)
+    else
+      return quot.round(1)
+    end
+  end
+
+  def outputValue(isNegative, quot, base)
+    ret = "$#{quot}" + base
+    if(isNegative)
+      ret = '(' + ret + ')'
+    end
+    return ret
+  end
+      
+
+  isNegative = false
+  num = num + 0.0
+  if(num < 0)
+    isNegative = true
+    num = -num
+  end
+
+  if num > 10 ** 15
+    puts "input out of range, only support integers up to 100's of T's"
+    return false
+  end
+
+  if num > 10 ** 12
+    quot = roundQuotential(num / 10 ** 12)
+    return outputValue(isNegative, quot, 'T')
+  else 
+    if num > 10 ** 9
+        quot = roundQuotential(num / 10 ** 9)
+        return outputValue(isNegative, quot, 'B')
+    else 
+      if num > 10 ** 6 
+          quot = roundQuotential(num / 10 ** 6)
+          return outputValue(isNegative, quot, 'M')
+      else 
+        if num > 10 ** 3 
+            quot = roundQuotential(num / 10 ** 3)
+            return outputValue(isNegative, quot, 'K')
+        else
+            return outputValue(isNegative, num, '')
+        end
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  puts numberToCurrency(0.0)
+  puts numberToCurrency(0)
+  puts numberToCurrency(140)
+  puts numberToCurrency(1240)
+  puts numberToCurrency(78192)
+  puts numberToCurrency(501200)
+  puts numberToCurrency(5853010238)
+  puts numberToCurrency(-1240123)
+  puts numberToCurrency(1115853010238)
+  puts numberToCurrency(111585301023822)
+  puts numberToCurrency(1115853010238222)
+  puts numberToCurrency(-111585301023822)
+end
