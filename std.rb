594ce92e764e8ebe2d17c1b8b6bf3355067baf52
yeyangever <yeyangever@gmail.com>
2013-01-24 21:40:45 -0800

    fix the floating point problem, handle case where input is not array, but not handle case where input contains non-number type

--- std.rb
+++ std.rb
@@ -1,14 +1,34 @@
 def std(array)
-    sum = 0
-    sumSqures = 0
-    array.each do |elem|
-        sum += elem
-        sumSqures += elem * elem
+    sum = 0.0
+    sumSqures = 0.0
+    if array.respond_to?("each")
+        array.each do |elem|
+            sum += elem
+            sumSqures += elem * elem
+        end
+        mean = sum / array.length
+        variance = 0.0
+        array.each do |elem|
+            variance += (elem - mean) ** 2
+        end
+        return Math.sqrt(variance / array.length)
+    else
+        puts "Input #{@array} is not an array, abort"
+        return -1
     end
-    mean = sum / array.length
-    variance = 0
-    array.each do |elem|
-        variance += (elem - mean) ** 2
-    end
-    return Math.sqrt(variance / array.length)
+end
+
+
+if __FILE__ == $0
+    array = [1, 1, 1]
+    puts std(array)
+
+    array = [1, 2, 3]
+    puts std(array)
+
+    array = 1
+    puts std(array)
+    
+    array = ["a", "b", "c"]
+    puts std(array)
 end
